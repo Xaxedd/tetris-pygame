01f505a4d4f8157b92e7373c35fa02db8537e28e
Sebaa <sebastiankostr@gmail.com>
2023-02-15 03:57:19 +0100

    added more colors

--- classes/colors.py
+++ classes/colors.py
@@ -8,3 +8,8 @@ class Colors:
     grey = [142, 147, 156]
     yellow = [230, 172, 37]
     light_blue = [36, 209, 186]
+    red = [255, 0, 0]
+    orange = [224, 99, 27]
+    dark_blue = [38, 43, 189]
+    lime = [18, 219, 41]
+    pink = [219, 18, 209]
